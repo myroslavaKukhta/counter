12c72242dfcbffb375e3cd655777b521f1d7c19a
Myroslava Kukhta <107687502+myroslavaKukhta@users.noreply.github.com>
2023-12-16 18:10:31 +0200

    Add min&max value

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import {Counter} from "./Counter";
-import {CounterPropsType} from "./Counter";
-import {Setting} from "./Setting";
-
-
+import { Counter } from './Counter';
+import { Setting, SettingPropsType } from './Setting';
 
 function App() {
-    const counterProps: CounterPropsType = {
-        maxValue: 5,
-        startValue: 0,
-    };
+    const [count, setCount] = useState(0);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const [maxValue, setMaxValue] = useState(10); // Дефолтне максимальне значення
+    const [startValue, setStartValue] = useState(0);
 
+    const onSetHandler: SettingPropsType['onSetHandler'] = (newMaxValue, newStartValue) => {
+        if (newMaxValue < 0 || newStartValue < 0 || newStartValue >= newMaxValue) {
+            setErrorMessage('Invalid input! Please enter non-negative numbers.');
+        } else {
+            setMaxValue(newMaxValue);
+            setStartValue(newStartValue);
+            setCount(newStartValue);
+            setErrorMessage(null);
+        }
+    };
     return (
         <div className="App">
-            <Setting {...counterProps}/>
-            <Counter {...counterProps} />
+            <Setting onSetHandler={onSetHandler} />
+            <Counter
+                maxValue={maxValue}
+                startValue={startValue}
+                errorMessage={errorMessage}
+                count={count}
+            />
         </div>
     );
 }
